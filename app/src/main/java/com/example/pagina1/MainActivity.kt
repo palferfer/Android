612c13c439091abb48e19c113c8c32edb197a6d2
palferfer <palff98@gmail.com>
2023-10-11 13:56:55 +0200

    Cambios

--- app/src/main/java/com/example/pagina1/MainActivity.kt
+++ app/src/main/java/com/example/pagina1/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Jetpack Compose tutorial")
+                    Greeting()
                 }
             }
         }
@@ -43,7 +43,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.bg_compose_background)
 
     Column(){
@@ -53,9 +53,20 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         )
 
         Text(
-            text = name,
+            text = "Jetpack Compose tutorial",
             modifier = Modifier.padding(16.dp),
-            textAlin
+            fontSize = 24.sp
+        )
+
+        Text(
+            text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+
+        Text(
+            text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+            modifier = Modifier.padding(16.dp)
         )
     }
 
@@ -77,6 +88,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Pagina1Theme {
-        Greeting("Jetpack Compose tutorial")
+        Greeting()
     }
 }
