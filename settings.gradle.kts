f99ce4a29ac89e1e8822ad0e64885518b587610b
palferfer <palff98@gmail.com>
2023-10-11 13:32:45 +0200

    Primeros cambios

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Pagina1"
+include(":app")
+ 
